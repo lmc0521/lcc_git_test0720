08c891b723b13c6433b5e6512744009c0eb681fb
minchih <cooker0521@gmail.com>
2024-07-13 14:41:38 +0800

    新增第幾次猜對功能

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 import random
 
 x = random.randint(1, 50)
-
+n = 0
 for i in range(5):
     y = int(input("請猜一個數字"))
-
+    n += 1
     if x == y:
-        print("bingo")
+        print(f"bingo,第{n}次猜對")
         break
     else:
         if x > y:
