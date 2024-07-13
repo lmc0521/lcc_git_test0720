78e9e288b2d6530721129764bbea848e7e33d629
minchih <cooker0521@gmail.com>
2024-07-13 14:07:19 +0800

    修改game.py成無線迴圈

--- game.py
+++ game.py
@@ -2,10 +2,10 @@ import random
 
 x = random.randint(1, 50)
 
-# y = int(input("請猜一個數字"))
-for i in range(5):
-    print(i)
-    # if x == y:
-    #     print("bingo")
-    # else:
-    #     print("猜錯了")
+while 1:
+    y = int(input("請猜一個數字"))
+
+    if x == y:
+        print("bingo")
+    else:
+        print("猜錯了")
