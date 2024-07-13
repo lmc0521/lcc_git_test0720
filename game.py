fe6c44ca80b80586ab0c1e7ddc35e8654acc3154
minchih <cooker0521@gmail.com>
2024-07-13 14:30:58 +0800

    新增大小功能

--- game.py
+++ game.py
@@ -2,11 +2,17 @@ import random
 
 x = random.randint(1, 50)
 
-while 1:
+for i in range(5):
     y = int(input("請猜一個數字"))
 
     if x == y:
         print("bingo")
         break
     else:
-        print("猜錯了")
+        if x > y:
+            print("猜高一點")
+        else:
+            print("猜低一點")
+
+if x != y:
+    print(f"答案為{x}")
