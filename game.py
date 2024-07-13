2ca5f8e886defdf4e8b5d050b045ff640c7f1aed
minchih <cooker0521@gmail.com>
2024-07-13 14:06:02 +0800

    新增game.py

---
+++ game.py
@@ -0,0 +1,11 @@
+import random
+
+x = random.randint(1, 50)
+
+# y = int(input("請猜一個數字"))
+for i in range(5):
+    print(i)
+    # if x == y:
+    #     print("bingo")
+    # else:
+    #     print("猜錯了")
